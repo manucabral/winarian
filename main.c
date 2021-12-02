73f9f55f976c79d2decdc83462363af5888acc20
Manu <mcabral@frba.utn.edu.ar>
2021-12-02 12:14:43 -0300

    Actualizado archivo principal

--- main.c
+++ main.c
@@ -10,11 +10,9 @@ int main(int argc, char *argv[])
     AjustarVentana();
     EstablecerTituloConsola();
 
-<<<<<<< HEAD
-    for (size_t i = 1; i <= 3; i++) ActualizarRegistro(&Lista, i);
+    for (size_t i = 1; i <= 5; i++) ActualizarRegistro(&Lista, i);
     system("cls");
-=======
->>>>>>> f48dff8d1faf68e7a9cc45eb89db8e58175ccba8
+    
     while (1)
     {
         MostrarSubtitulo();
